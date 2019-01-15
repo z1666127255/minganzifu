0f4404bef62cb37e96e6f4f8c308167a12eb1508
曾英峰 <1666127255@qq.com>
2019-01-15 17:01:08 +0800

    基本框架结果

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 
+char arr[1024];
+int index = 0;
+
 int main(){
 
-    printf("hello world!");
+    printf("minganzifuguanlixitong\n");
+
+    while(1){
+        printf("1----tianjia\n");
+        printf("2----shanchu\n");
+        printf("3----chakansuoyou\n");
+        printf("4----tihuan\n");
+        printf("5----tuichu\n");
+
+        printf("xuanzegongneng\n");
+
+        int code;
+        scanf("%c",&code);
+        scanf("%c",&code);
+
+        if(code == 1){
+
+        }
+        if(code == 2){
+            
+        }
+        if(code == 3){
+            
+        }
+        if(code == 4){
+            
+        }
+        if(code == 5){
+            
+        }
+    }
 
     return 0;
 
