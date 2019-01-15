1cf4cab6ebe53f7b353ff39ea58e53a1aa4b3c89
曾英峰 <1666127255@qq.com>
2019-01-15 19:50:44 +0800

    敏感字符替代

--- main.c
+++ main.c
@@ -2,6 +2,23 @@
 
 char arr[1024];
 int index = 0;
+int isMingan(char x){
+
+    int count = 0;
+
+    for(int i=0;i<index;i++){
+        if(x == arr[i]){
+            count++;
+        }
+    }
+
+    if(count == 0){
+        return 0;
+    }else{
+        return 1;
+    }
+
+}
 
 int main(){
 
@@ -57,7 +74,30 @@ int main(){
             scanf("%c", &x);
         }
         if(code == 4){
-            
+            printf("qingshuruyiduanwenzi\n");
+            char neirong[1024];
+            scanf("%s",neirong);
+
+            for(int i = 0;1;i++){
+                if(neirong[i] == '\0'){
+                    break;
+                }
+
+                if(isMingan(arr[i]) == 1){
+                    neirong[i] = '*';
+                }
+
+                printf("%c",neirong[i]);
+
+
+            }
+
+            printf("\n");
+
+            printf("替换完成，点击回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if(code == 5){
             
